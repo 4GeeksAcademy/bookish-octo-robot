d79d3ce4d8889e22a90eb6f976c7138ebc032545
carolinaborr <162605159+carolinaborr@users.noreply.github.com>
2024-10-23 15:43:57 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // arrays
+  let pronoun = ["the", "our", "his"];
+  let adj = ["satanic", "infernal", "rotten"];
+  let noun = ["priest", "nun", "corpse", "murder", "mass"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        document.querySelector("#domain").innerHTML =
+          pronoun[i] + adj[j] + noun[k] + ".com";
+
+        let domainparagraph = document.createElement("p");
+        domainparagraph.textContent = pronoun[i] + adj[j] + noun[k] + ".com";
+        let divelement = document.querySelector("#container");
+        divelement.appendChild(domainparagraph);
+      }
+    }
+  }
 };
